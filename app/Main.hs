2bab9a29329ebda79ea38dec84b984242aff8154
Alexandre Delanoë <anoe@debian.org>
2017-05-15 11:16:18 +0200

    [OPT] Adding command line options.

--- app/Main.hs
+++ app/Main.hs
@@ -2,18 +2,54 @@ module Main where
 
 import System.Environment (getArgs)
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+
 import Safe (headMay)
-import Text.Parsec
+import qualified Text.Parsec as TP
 import Data.BoolExpr
 import Data.BoolExpr.Parser
 import Data.BoolExpr.Printer
 
+
+
+data Options = Options { query :: String
+                       , count :: Bool
+                       , limit :: Int
+                       } deriving (Show)
+
+options :: Parser Options
+options = Options
+        <$> strOption ( long "query"
+                      <> short 'q'
+                      <> metavar "TARGET"
+                      <> help "Query as boolean language."
+                       )
+        
+        <*> switch    ( long "count"
+                      <> short 'c'
+                      <> help "Count (True) or False get"
+                       )
+        
+        <*> option auto ( long "Limit of results"
+                        <> short 'l'
+                        <> value 1000
+                        <> metavar "INT"
+                         )
+
+queryTest0 :: String
 queryTest0 = "(a OR b) AND (n OR m) NOT (x OR y)"
+
+queryTest1 :: String
 queryTest1 = "a OR b"
 
+
+contains :: Show a => a -> ShowS
 contains  x = showString ("( bif:contains(?title, "++ show x ++")"
                        ++ " OR bif:contains(?abstract, "++ show x ++"))"
                          )
+
+begin_query :: String
 begin_query = unlines ["select ?resource ?id ?title ?date ?abstract ?source where {"
                       ,"?resource a isidore:BibliographicalResource;"
                       ,"dcterms:title ?title;"
@@ -25,19 +61,31 @@ begin_query = unlines ["select ?resource ?id ?title ?date ?abstract ?source wher
                       ,"FILTER"
                       ]
 
-end_query = unlines ["}"
-                    ,"limit 100"
+end_query :: Int -> String
+end_query n = unlines ["}"
+                    ,"limit " ++ show n
                     ]
 
-create query = begin_query ++ cnfPrinter (contains) (fromCNF (boolTreeToCNF query')) end_query
+createQuery :: [Char] -> Int -> [Char]
+createQuery query n = begin_query ++ cnfPrinter (contains) (fromCNF (boolTreeToCNF query')) (end_query n)
     where
-        query' = case runParser (parseBoolExpr identifier) () "" query of
+        query' = case TP.runParser (parseBoolExpr identifier) () "" query of
           Right query'' -> query''
-          Left n       -> error "BUG at Parsing"
+          Left  bad     -> error $ "BUG at Parsing" ++ (show bad)
+
+
+
+bool2sparql :: Options -> IO ()
+bool2sparql (Options q False n) = putStrLn $ createQuery q n
+bool2sparql (Options q True  _) = putStrLn $ "count only" ++ q
+
 
 main :: IO ()
-main = do
-    q <- getArgs
-    case headMay q of
-      Nothing    -> putStrLn "Enter your query please"
-      Just query -> putStrLn (create query)
+main = bool2sparql =<< execParser opts
+  where
+    opts = info (options <**> helper)
+      ( fullDesc
+     <> progDesc "Print your query in SPARQL language"
+     <> header "bool2sparql - DSL to translate boolean query to Sparql query." )
+
+
