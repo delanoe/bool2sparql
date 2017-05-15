8b7054b51f0a17de19e1a8163e34ea83c788394d
Alexandre Delanoë <anoe@debian.org>
2017-05-15 12:43:58 +0200

    SPLIT code

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,8 @@ import System.Environment (getArgs)
 import Options.Applicative
 import Data.Semigroup ((<>))
 
-import Safe (headMay)
-import qualified Text.Parsec as TP
-import Data.BoolExpr
-import Data.BoolExpr.Parser
-import Data.BoolExpr.Printer
-
+-- import Safe (headMay)
+import Data.Bool2sparql (createQuery)
 
 
 data Options = Options { query :: String
@@ -36,44 +32,6 @@ options = Options
                         <> value 1000
                         <> metavar "INT"
                          )
-
-queryTest0 :: String
-queryTest0 = "(a OR b) AND (n OR m) NOT (x OR y)"
-
-queryTest1 :: String
-queryTest1 = "a OR b"
-
-
-contains :: Show a => a -> ShowS
-contains  x = showString ("( bif:contains(?title, "++ show x ++")"
-                       ++ " OR bif:contains(?abstract, "++ show x ++"))"
-                         )
-
-begin_query :: String
-begin_query = unlines ["select ?resource ?id ?title ?date ?abstract ?source where {"
-                      ,"?resource a isidore:BibliographicalResource;"
-                      ,"dcterms:title ?title;"
-                      ,"dcterms:date ?date;"
-                      ,"dc:description ?abstract;"
-                      ,"dc:description ?id;"
-                      ,"ore:isAggregatedBy ?s."
-                      ,"?s dcterms:title ?source."
-                      ,"FILTER"
-                      ]
-
-end_query :: Int -> String
-end_query n = unlines ["}"
-                    ,"limit " ++ show n
-                    ]
-
-createQuery :: [Char] -> Int -> [Char]
-createQuery query n = begin_query ++ cnfPrinter (contains) (fromCNF (boolTreeToCNF query')) (end_query n)
-    where
-        query' = case TP.runParser (parseBoolExpr identifier) () "" query of
-          Right query'' -> query''
-          Left  bad     -> error $ "BUG at Parsing" ++ (show bad)
-
-
 
 bool2sparql :: Options -> IO ()
 bool2sparql (Options q False n) = putStrLn $ createQuery q n
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+
+queryTest0 :: String
+queryTest0 = "(a OR b) AND (n OR m) NOT (x OR y)"
+
+queryTest1 :: String
+queryTest1 = "a OR b"
+
+
